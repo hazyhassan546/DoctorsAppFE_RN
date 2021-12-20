ee2fba9a4f84596badeda6ec8a2ecedf919c2d41
Hassan Abbas <hazzyhassan888@gmail.com>
2021-12-20 10:25:59 +0500

    new images added

--- src/common/images.js
+++ src/common/images.js
@@ -1,3 +1,25 @@
-// const babyImage = require('../assets/babyboth.png');
+const heart = require('../assets/heart-svg.png');
+const confirm = require('../assets/confirm.png');
+const timer = require('../assets/timer.png');
+const monitor = require('../assets/monitor.png');
+const stetho = require('../assets/stetho.png');
+const drop = require('../assets/drop.png');
+const teeth = require('../assets/teeth.png');
+const docBg = require('../assets/doctorBackground.png');
+const calendar = require('../assets/calendar.png');
+const corona = require('../assets/corona.png');
+const doctor = require('../assets/doctor.png');
 
-export default Images = {};
+export default Images = {
+  heart,
+  confirm,
+  timer,
+  monitor,
+  stetho,
+  drop,
+  teeth,
+  docBg,
+  calendar,
+  corona,
+  doctor,
+};
