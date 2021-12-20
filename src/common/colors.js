f7c35d7451351dcd47cd78d9ad6ee37a13bb80f2
Hassan Abbas <hazzyhassan888@gmail.com>
2021-12-20 10:31:37 +0500

    App theme Colors added

--- src/common/colors.js
+++ src/common/colors.js
@@ -1,23 +1,21 @@
 const COLORS = {
-  WHITE: "#ffffff",
-  BLACK: "#000000",
-  APP_BLUE: "#2982BC",
-  SIDE_MENU_TEXT: "#7E7E7E",
-  REDISH: "#E46A6A",
-  PINKISH: "#E49F9F",
-  LIGHT_BLUE: "#2BAFF6",
-  SKY_BLUE: "#DDEBF2",
+  WHITE: '#ffffff',
+  BLACK: '#000000',
+  PRIMARY: '#0FD3C4',
+  SECONDARY: '#E7FAF9',
+  GREEN_FONT: '#FF6A6A',
+  PRIMARY_GREY: '#B9B9B9',
 };
 
-const lighten = (value) => {
+const lighten = value => {
   const MAX_HEX_VALUE = 255;
   const hexValue = Math.floor(MAX_HEX_VALUE * Math.min(value, 1)).toString(16);
   return hexValue.length < 2 ? `0${hexValue}` : hexValue;
 };
 
 const alpha = (color, value) =>
-  color.startsWith("#") ? `${color}${lighten(value)}` : color;
+  color.startsWith('#') ? `${color}${lighten(value)}` : color;
 
-export { alpha, lighten };
+export {alpha, lighten};
 
 export default COLORS;
